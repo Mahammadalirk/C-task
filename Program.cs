103d0fe4ae31c29ff8683fb7feaea10da51fdc04
mahammadalirk <mahammadalirk@code.edu.az>
2022-06-20 01:15:02 +0400

    Program.cs file

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//int n = 21;
+//if (n % 3 == 0 && n % 7 == 0)
+//    Console.WriteLine(n);
+//else
+//    Console.WriteLine("false");
+//int n = 18;
+//    int m= 20;
+//if (n % 2 == 0 && m % 2 == 0)
+//    Console.WriteLine(n + m);
+//else;
+//Console.WriteLine("false");
+//int n = 15;
+//int m = 25;
+//int sum = 0;
+//for (int i = n + 1; i < m; i++) ;
+
+
+//if (i % 2 == 1 && n < m) ;
+
+//sum += i;
+
+//Console.WriteLine(sum);
+
+//int[] num = { 6, 7, 9, 12,14, 19, 22, 25, 27, 28, 30 };
+//int sum = 0;
+//for (int i = 0; i < num.Length; i++) ;
+//if (num[i]  % 2 == 1) ;
+//sum += num[i];
+//Console.WriteLine(sum);
+
+//int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 16, 22, 35, 46, 47, 50 };
+//int count = 0;
+//for (int i = 0; i < num.Length; i++) ;
+//if (num[i] % 2 == 0) ;
+//count++;
+//Console.WriteLine(count);
